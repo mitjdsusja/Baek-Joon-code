d6a9e03f9708e8f1aa7dab150788bc09fcf50dd3
mitjdsusja <53611797+mitjdsusja@users.noreply.github.com>
2020-03-27 18:28:52 +0900

    BOJ #4153 직각삼각형 <완료> 2020-03-27

---
+++ 4153.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#define SIZE 1000
+
+
+int main() {
+	int x, y, z;
+	int a[3];
+	int max = 0;
+	int sum = 0;
+
+	for (;;) {
+		scanf("%d %d %d", &a[0], &a[1], &a[2]);
+		for (int i = 0; i < 3; i++) {
+			if (a[i] == 0) {
+				return 0;
+			}
+		}
+		for (int i = 0; i < 3; i++) {
+			if (max < a[i]) {
+				max = a[i];
+			}
+		}
+		for (int i = 0; i < 3; i++) {
+			a[i] = pow(a[i], 2);
+		}
+		max = pow(max, 2);
+
+		for (int i = 0; i < 3; i++) {
+			if (max != a[i]) {
+				sum += a[i];
+			}
+		}
+		if (max == sum) {
+			printf("right\n");
+		}
+		else {
+			printf("wrong\n");
+		}
+		sum = 0;
+		max = 0;
+	}
+}
